8294f2c9af66f25c8cc1b05f284b563531c0d699
kobakichi <fenderstrat57vin@gmail.com>
2022-05-21 23:52:14 +0900

    firstcommit

---
+++ src/components/InputForm.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+
+export const InputForm: React.FC = () => {
+  return (
+    <>
+      <Form className="input-form label">
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder="First Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder="Last Name" />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label="I agree to the Terms and Conditions" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </>
+  );
+};
