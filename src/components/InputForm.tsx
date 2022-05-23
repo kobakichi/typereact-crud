ceb1e7209ef77f67e89133249af49f31fcee447e
kobakichi <fenderstrat57vin@gmail.com>
2022-05-24 05:34:00 +0900

    postDataの関数作成。

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Checkbox, Form } from "semantic-ui-react";
 
 export const InputForm: React.FC = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  // チェックボックス用ステート
+  const [checkbox, setCheckbox] = useState(false);
+
+  // apiへデータを送信する関数
+  const postData = () => {
+    console.log(firstName);
+    console.log(lastName);
+    console.log(checkbox);
+  };
+
   return (
     <>
       <Form className="input-form label">
         <Form.Field>
           <label>First Name</label>
-          <input placeholder="First Name" />
+          <input
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </Form.Field>
         <Form.Field>
           <label>Last Name</label>
-          <input placeholder="Last Name" />
+          <input
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </Form.Field>
         <Form.Field>
-          <Checkbox label="I agree to the Terms and Conditions" />
+          <Checkbox
+            label="I agree to the Terms and Conditions"
+            onChange={(e) => setCheckbox(!checkbox)}
+          />
         </Form.Field>
-        <Button type="submit">Submit</Button>
+        <Button onClick={postData} type="submit">
+          api送信
+        </Button>
       </Form>
     </>
   );
